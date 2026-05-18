018313f94024cb4f8a3e139f6c4de11354b14af1
Morbanaa <theodorerodd@gmail.com>
2026-05-18 15:19:11 -0500

    Set up main creating a game manager object and calling the method world gen. In the loop it draws the game and resets the screen each game tick. The game speed will run based on const GAME_SPEED

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-ECHO is on.
+# Teddy Rodd
+# Morbanaa Studios
+# Pong
+
+from game_manager import Game_Manger
+import time
+
+def main():
+    game_manager = Game_Manger(25,80) # height then width
+    GAME_SPEED = .05
+
+    game_manager.world_gen()
+    while True:
+        # Render
+        game_manager.render_world()
+
+        # Util
+        time.sleep(GAME_SPEED)
+        game_manager.clear_move_cursor()
+
+
+if __name__ == "__main__":
+    main()
