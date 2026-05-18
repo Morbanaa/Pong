835615a6b14bf790d9afc06932103c8b91e559a3
Morbanaa <theodorerodd@gmail.com>
2026-05-18 15:40:30 -0500

    render world now draws the player. Moved player to side of screen like in pong

--- game_manager.py
+++ game_manager.py
@@ -3,6 +3,7 @@
 # Pong
 
 import sys
+from player import Player
 
 # Colors
 BLACK   = '\033[30m'
@@ -16,13 +17,16 @@ WHITE   = '\033[37m'
 LIGHT_GRAY = '\033[37m'
 DARK_GRAY  = '\033[90m'
 # Reset Color
-ENDC = '\033[0m'
+RESET = '\033[0m'
 
 class Game_Manger():
     def __init__(self,game_height,game_width):
         self.game_height = game_height
         self.game_width = game_width
         self.game_map = []
+
+        # Creates Player Object
+        self.player = Player(self.game_height//2,3)
 
     def world_gen(self):
         for y in range(self.game_height):
@@ -37,7 +41,10 @@ class Game_Manger():
     def render_world(self):
         for y in range(self.game_height):
             for x in range(self.game_width):
-                print(f"{DARK_GRAY}{self.game_map[y][x]}{ENDC}",end="")
+                if y == self.player.ypos and x == self.player.xpos:
+                    print(f"{GREEN}|{RESET}",end="")
+                else:
+                    print(f"{DARK_GRAY}{self.game_map[y][x]}{RESET}",end="")
             print()
 
     def clear_move_cursor(self):
