9c2a7a341ebfcef7567b808988e433304c3843ae
Morbanaa <theodorerodd@gmail.com>
2026-05-18 17:08:25 -0500

    Added ai movement. This will be removed onces collisons with ball are done and ai will be used for this paddles movement

--- ai.py
+++ ai.py
@@ -1,4 +1,12 @@
+import keyboard
+
 class Ai():
     def __init__(self,ypos,xpos):
         self.ypos = ypos
         self.xpos = xpos
+
+    def update_ai(self,game_map):
+        if keyboard.is_pressed("UP") and game_map[self.ypos -3][self.xpos] != "@":
+            self.ypos -= 1
+        if keyboard.is_pressed("DOWN") and game_map[self.ypos +3][self.xpos] != "@":
+            self.ypos += 1
