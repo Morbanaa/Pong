11623bb8e6ef651c99b7dab6a7d7396c4c20dc92
Morbanaa <theodorerodd@gmail.com>
2026-05-18 17:07:35 -0500

    Added player movement

--- player.py
+++ player.py
@@ -1,4 +1,12 @@
+import keyboard
+
 class Player():
     def __init__(self,ypos,xpos):
         self.ypos = ypos
         self.xpos = xpos
+
+    def update_player(self,game_map):
+        if keyboard.is_pressed("W") and game_map[self.ypos -3][self.xpos] != "@":
+            self.ypos -= 1
+        if keyboard.is_pressed("S") and game_map[self.ypos +3][self.xpos] != "@":
+            self.ypos += 1
