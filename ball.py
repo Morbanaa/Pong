b775cb8700d036cd178e2c01e915669e0cc3ebf6
Morbanaa <theodorerodd@gmail.com>
2026-05-18 22:11:12 -0500

    Added collisons with walls and paddles. The angles need more work so the ball goes the right way each time...

--- ball.py
+++ ball.py
@@ -4,27 +4,54 @@ class Ball():
     def __init__(self,ypos,xpos):
         self.ypos = ypos
         self.xpos = xpos
-        self.yDir = random.randint(-2,2)
-        self.xDir = random.randint(0,1)
+        self.yDir = random.randint(-1,1)
+        self.xDir = random.randint(1,2)
         self.counter = 0
 
-    def update_ball(self,game_map):
+    def update_ball(self,game_map,player,ai,game_height,game_width):
 
         if self.counter %2 == 0:
-            if self.xDir == 1:
-               self.xpos += 1
-            elif self.xDir == 0:
-                self.xpos -=1
+            # Resets Ball 
+            if self.xpos < player.xpos or self.xpos > ai.xpos:
+                self.xpos = game_width//2
+                self.ypos = game_height//2
+                self.yDir = random.randint(-1,1)
+                self.xDir = random.randint(1,2)
+            
+            # Hit Left Paddle
+            if abs(self.ypos - player.ypos) < 3 and abs(self.xpos - player.xpos) < 1:
+                self.xDir = 2
+                if abs(self.ypos - player.ypos) > 0 and abs(self.ypos - player.ypos) < 3:
+                    self.yDir = -1
+                elif abs(self.ypos - player.ypos) < 0 and abs(self.ypos - player.ypos) > -3:
+                    self.yDir = 1
+                
+            # Hit Right Paddle
+            if abs(self.ypos - ai.ypos) < 3 and abs(self.xpos - ai.xpos) < 1:
+                self.xDir = 1
+                if abs(self.ypos - ai.ypos) > 0 and abs(self.ypos - ai.ypos) < 3:
+                    self.yDir = -1
+                elif abs(self.ypos - ai.ypos) < 0 and abs(self.ypos - ai.ypos) > -3:
+                    self.yDir = 1
+            
+            # Hit Wall
+            if game_map[self.ypos][self.xpos] == "@":
+                if self.yDir == -1:
+                    self.yDir = 1
+                elif self.yDir == 1:
+                    self.yDir = -1
 
+            # Left Right
+            if self.xDir == 1:
+               self.xpos -= 1
+            elif self.xDir == 2:
+                self.xpos +=1
+
+            # Up Down
             if self.yDir != 0:
-                if self.yDir == -2:
-                    self.ypos -=2
-                elif self.yDir == -1:
+                if self.yDir == -1:
                     self.ypos -=1
                 elif self.yDir == 1:
                     self.ypos +=1
-                elif self.yDir == 2:
-                    self.ypos += 2
-
+                
         self.counter += 1
-         
